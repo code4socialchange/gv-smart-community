21cd2fe895bbccd1a8819cafbb1c258e3a75c7c0
Code4SocialChange <code4socialchange@gmail.com>
2019-09-21 11:15:30 +0530

    Added blog and village

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+export interface PeriodicElement {
+  name: string;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Hydrogen', weight: 1.0079 },
+  {name: 'Helium', weight: 4.0026 },
+  {name: 'Lithium', weight: 6.941 },
+  {name: 'Beryllium', weight: 9.0122 },
+  {name: 'Boron', weight: 10.811 },
+  {name: 'Carbon', weight: 12.0107 },
+  {name: 'Nitrogen', weight: 14.0067 },
+  {name: 'Oxygen', weight: 15.9994 },
+  {name: 'Fluorine', weight: 18.9984 },
+  {name: 'Neon', weight: 20.1797 },
+];
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +29,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  displayedColumns: string[] = ['select', 'name', 'weight'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
 
 }
