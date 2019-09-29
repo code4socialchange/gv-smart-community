3a63af1401ec7ec7b334d643191f6a921bc93b64
Code4SocialChange <code4socialchange@gmail.com>
2019-09-29 12:36:08 +0530

    minor

--- api/app.js
+++ api/app.js
@@ -20,13 +20,13 @@ app.use(helmet());
 app.use(cors());
 
 if (process.env.SERVERTYPE == 'OFFLINE') {
-    app.use('/api', require('./offline/routes'));
+	app.use('/api', require('./offline/routes'));
 } else {
-    app.use('/api', require('./server/routes'));
+	app.use('/api', require('./server/routes'));
 }
 
 models.sequelize.sync().then(() => {
-    app.listen(3000, () => logger.info(`Server started successfully on port 3000`));
+	app.listen(3000, () => logger.info(`Server started successfully on port 3000`));
 }).catch((error) => {
-    logger.error('Error', error.message)
+	logger.error('Error', error.message)
 });
