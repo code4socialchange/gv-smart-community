c802fad8f5e2f479b876a4f2b5c3d75c2e2bcf45
Code4SocialChange <code4socialchange@gmail.com>
2019-10-05 21:21:15 +0530

    added village

--- src/app/components/village/village.component.ts
+++ src/app/components/village/village.component.ts
@@ -1,32 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 import { SharedService } from 'src/app/services/shared.service';
 import { Observable } from 'rxjs';
 import { map } from 'bluebird';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-village',
@@ -38,38 +19,26 @@ export class VillageComponent implements OnInit {
   villageForm: FormGroup;
   villages: Array<any> = [];
 
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['id', 'name', 'district', 'pincode'];
+  dataSource = new MatTableDataSource<any>();
+  selection = new SelectionModel<any>(true, []);
 
+  @ViewChild(MatTable, null) table: MatTable<any>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild('ngForm', null) ngForm: NgForm;
 
   constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
+    this.getVillages();
+    this.createVillageForm();
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    this.createVillageForm();
-    console.log(this.villageForm.valid && this.villageForm.touched)
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 
   createVillageForm() {
@@ -82,22 +51,22 @@ export class VillageComponent implements OnInit {
 
   getVillages() {
     this.sharedService.getVillages().subscribe(res => {
-      this.villages = res['villages'];
-      this.sharedService.villages = res['villages'];
+      this.sharedService.villages.next(res['villages']);
+      this.dataSource.data = res['villages'];
     })
   }
 
   saveVillage() {
     if (this.villageForm.invalid) return;
-    this.villageForm.markAsPristine()
+    this.villageForm.markAsPristine()    
     this.sharedService.addVillage(this.villageForm.value).subscribe(_ => {
-      this.villageForm.reset();
+      this.ngForm.resetForm();
       this.getVillages();
     });
   }
 
   resetVillageForm() {
-    this.villageForm.reset();
+    this.ngForm.resetForm();
   }
 
 }
